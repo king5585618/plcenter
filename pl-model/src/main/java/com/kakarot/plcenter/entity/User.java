83ca3877aa66a921bab3f1babc224c564129860c
jinzj <346952524@qq.com>
2017-02-15 17:56:24 +0800

    可以启动了

--- pl-model/src/main/java/com/kakarot/plcenter/entity/User.java
+++ pl-model/src/main/java/com/kakarot/plcenter/entity/User.java
@@ -2,9 +2,11 @@ package com.kakarot.plcenter.entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.rest.core.annotation.Description;
 
-import javax.persistence.*;
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
 
 /**
  * Created by jinzj on 2017/1/20.
@@ -16,26 +18,21 @@ import javax.persistence.*;
 @Cacheable(value = false)
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(length = 20,nullable = false)
-    @Description("用户名")
     private String username;
 
-    @Column(length = 50,nullable = false)
-    @Description("密码")
     private String password;
 
-    @Column(length = 50)
-    @Description("昵称")
-    private String nickName;
+    private String state;
+
+    private Date createTime;
 
 //    @CollectionTable(name = "SSO_User_AuthorityList")
 //    @ElementCollection
 //    @Column(length = 50)
 //    private Set<String> authorityList;
+
 
     @Override
     public boolean equals(Object o) {
@@ -44,21 +41,21 @@ public class User {
 
         User user = (User) o;
 
-        if (!id.equals(user.id)) return false;
-        if (!username.equals(user.username)) return false;
-        if (!password.equals(user.password)) return false;
-        if (nickName != null ? !nickName.equals(user.nickName) : user.nickName != null) return false;
-//        return authorityList != null ? authorityList.equals(user.authorityList) : user.authorityList == null;
-        return true;
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (username != null ? !username.equals(user.username) : user.username != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        if (state != null ? !state.equals(user.state) : user.state != null) return false;
+        return createTime != null ? createTime.equals(user.createTime) : user.createTime == null;
+
     }
 
     @Override
     public int hashCode() {
-        int result = id.hashCode();
-        result = 31 * result + username.hashCode();
-        result = 31 * result + password.hashCode();
-        result = 31 * result + (nickName != null ? nickName.hashCode() : 0);
-//        result = 31 * result + (authorityList != null ? authorityList.hashCode() : 0);
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (state != null ? state.hashCode() : 0);
+        result = 31 * result + (createTime != null ? createTime.hashCode() : 0);
         return result;
     }
 }
--- pl-service/src/main/java/com/kakarot/plcenter/DatasourceConfig.java
+++ pl-service/src/main/java/com/kakarot/plcenter/DatasourceConfig.java
@@ -1,52 +1,61 @@
 package com.kakarot.plcenter;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.kakarot.plcenter.util.PropertyUtil;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.util.Properties;
 
 /**
  * Created by jinzj on 2017/2/13.
  */
 @Configuration
 @Profile("!production")
-@ImportResource("classpath:applicationContext.xml")
+//@ImportResource("classpath:spring-service.xml")
 public class DatasourceConfig {
 
 //    private static final Log log = LogFactory.getLog(DatasourceConfig.class);
 
-//    @Bean
-//    public DruidDataSource dataSource() throws Exception {
-//        Properties properties = PropertyUtil.getDatasourceProperties("datasource.properties");
-//        DruidDataSource source = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
-//        source.setConnectProperties(properties);
-//        return source;
-//    }
+    @Bean
+    public DruidDataSource dataSource() throws Exception {
+        Properties properties = PropertyUtil.getDatasourceProperties("datasource.properties");
+        DruidDataSource source = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
+        source.setConnectProperties(properties);
+        return source;
+    }
 
     /**
      * mybatis文件配置，扫描所有mapper文件
      * @throws Exception
      */
-//    @Bean(name = "sqlSessionFactory")
-//    public SqlSessionFactoryBean getSqlSessionFactoryBean() throws Exception {
-//        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-//        bean.setDataSource(dataSource());
-//        bean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
-//        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-//        bean.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
-//        return bean;
-//    }
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactoryBean getSqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        bean.setDataSource(dataSource());
+        bean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        bean.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
+        return bean;
+    }
 
     /**
      * spring与mybatis整合配置，扫描所有dao
      */
-//    @Bean
-//    public MapperScannerConfigurer mapperScannerConfigurer() {
-//        MapperScannerConfigurer configurer = new MapperScannerConfigurer();
-//        configurer.setBasePackage("com.kakarot.plcenter.dao");
-//        configurer.setSqlSessionFactoryBeanName("sqlSessionFactoryBean");
-//        return configurer;
-//    }
-//
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer configurer = new MapperScannerConfigurer();
+        configurer.setBasePackage("com.kakarot.plcenter.dao");
+        configurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+        return configurer;
+    }
+
 //    @Bean(name = "transactionManager")
 //    public DataSourceTransactionManager dataSourceTransactionManager() throws Exception {
 //        DataSourceTransactionManager manager = new DataSourceTransactionManager();
