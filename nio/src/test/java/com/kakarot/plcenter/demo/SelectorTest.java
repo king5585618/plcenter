d1b4db3362fd7b7a742c0e09d71d271cabeab797
jinzj <346952524@qq.com>
2017-02-24 14:28:55 +0800

    nio test

--- nio/src/test/java/com/kakarot/plcenter/demo/SelectorTest.java
+++ nio/src/test/java/com/kakarot/plcenter/demo/SelectorTest.java
@@ -3,8 +3,11 @@ package com.kakarot.plcenter.demo;
 import org.junit.Test;
 
 import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by jinzj on 2017/2/24.
@@ -17,6 +20,24 @@ public class SelectorTest {
         ServerSocketChannel socketChannel = ServerSocketChannel.open();
         socketChannel.configureBlocking(false);
         socketChannel.socket().bind(new InetSocketAddress(8080));
-
+        socketChannel.register(selector,SelectionKey.OP_ACCEPT| SelectionKey.OP_CONNECT|SelectionKey.OP_READ|SelectionKey.OP_WRITE);
+        while (true) {
+            selector.select();
+            Set<SelectionKey> selectionKeySet = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = selectionKeySet.iterator();
+            while(keyIterator.hasNext()) {
+                SelectionKey key = keyIterator.next();
+                if(key.isAcceptable()) {
+                    // a connection was accepted by a ServerSocketChannel.
+                } else if (key.isConnectable()) {
+                    // a connection was established with a remote server.
+                } else if (key.isReadable()) {
+                    // a channel is ready for reading
+                } else if (key.isWritable()) {
+                    // a channel is ready for writing
+                }
+                keyIterator.remove();
+            }
+        }
     }
 }
