475e8b3e5230109268b323bc22b52ab3838c3830
jinzj <346952524@qq.com>
2017-03-08 10:45:23 +0800

    线程

---
+++ thread/src/main/java/com/kakarot/plcenter/demo/NumberHolder.java
@@ -0,0 +1,135 @@
+package com.kakarot.plcenter.demo;
+
+/**
+ * Created by jinzj on 2017/3/7.
+ */
+public class NumberHolder {
+    private int number;
+
+    public synchronized void increase()
+    {
+        while (0 != number)
+        {
+            try
+            {
+                wait();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        // 能执行到这里说明已经被唤醒
+        // 并且number为0
+        number++;
+        System.out.println(number);
+
+        // 通知在等待的线程
+        notify();
+    }
+
+    public synchronized void decrease()
+    {
+        if (0 == number)
+        {
+            try
+            {
+                wait();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+
+        // 能执行到这里说明已经被唤醒
+        // 并且number不为0
+        number--;
+        System.out.println(number);
+        notify();
+    }
+
+    public static void main(String[] args)
+    {
+        NumberHolder numberHolder = new NumberHolder();
+
+        Thread t1 = new IncreaseThread(numberHolder);
+        Thread t2 = new DecreaseThread(numberHolder);
+        Thread t3 = new IncreaseThread(numberHolder);
+        Thread t4 = new DecreaseThread(numberHolder);
+
+        t1.start();
+        t2.start();
+
+        t3.start();
+        t4.start();
+    }
+
+}
+
+
+class IncreaseThread extends Thread
+{
+    private NumberHolder numberHolder;
+
+    public IncreaseThread(NumberHolder numberHolder)
+    {
+        this.numberHolder = numberHolder;
+    }
+
+    @Override
+    public void run()
+    {
+        for (int i = 0; i < 20; ++i)
+        {
+            // 进行一定的延时
+            try
+            {
+                Thread.sleep((long) Math.random() * 1000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+
+            // 进行增加操作
+            numberHolder.increase();
+        }
+    }
+
+}
+
+
+
+class DecreaseThread extends Thread
+{
+    private NumberHolder numberHolder;
+
+    public DecreaseThread(NumberHolder numberHolder)
+    {
+        this.numberHolder = numberHolder;
+    }
+
+    @Override
+    public void run()
+    {
+        for (int i = 0; i < 20; ++i)
+        {
+            // 进行一定的延时
+            try
+            {
+                Thread.sleep((long) Math.random() * 1000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+
+            // 进行减少操作
+            numberHolder.decrease();
+        }
+    }
+
+}
