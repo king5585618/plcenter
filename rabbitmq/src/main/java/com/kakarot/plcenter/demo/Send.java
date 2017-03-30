3e4d64f049ff91fa32c6853cb434857754a14092
jinzj <346952524@qq.com>
2017-03-30 20:33:15 +0800

    MQ

---
+++ rabbitmq/src/main/java/com/kakarot/plcenter/demo/Send.java
@@ -0,0 +1,33 @@
+package com.kakarot.plcenter.demo;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * Created by jinzj on 2017/3/30.
+ */
+public class Send {
+    //队列名称
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        //设置MabbitMQ所在主机ip或者主机名
+        factory.setHost("localhost");
+        //创建一个连接
+        Connection connection = factory.newConnection();
+        //创建一个频道
+        Channel channel = connection.createChannel();
+        //指定一个队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        //发送的消息
+        String message = "hello world!";
+        //往队列中发出一条消息
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+        //关闭频道和连接
+        channel.close();
+        connection.close();
+    }
+}
