90387b8085e272283f9cf391e27ccc55bff69aed
jinzj <346952524@qq.com>
2017-11-02 14:55:48 +0800

    Lock

---
+++ thread/src/main/java/com/kakarot/plcenter/lock/LockTest.java
@@ -0,0 +1,60 @@
+package com.kakarot.plcenter.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author jinzj
+ * @since v1.0.0
+ */
+public class LockTest {
+    private Lock lock = new ReentrantLock();
+    public static void main(String[] args)  {
+        LockTest test = new LockTest();
+        MyThread thread1 = new MyThread(test);
+        MyThread thread2 = new MyThread(test);
+        thread1.start();
+        thread2.start();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread2.interrupt();
+    }
+
+    public void insert(Thread thread) throws InterruptedException{
+        lock.lockInterruptibly();   //注意，如果需要正确中断等待锁的线程，必须将获取锁放在外面，然后将InterruptedException抛出
+        try {
+            System.out.println(thread.getName()+"得到了锁");
+            long startTime = System.currentTimeMillis();
+            for(    ;     ;) {
+                if(System.currentTimeMillis() - startTime >= 10000)
+                    break;
+                //插入数据
+            }
+        }
+        finally {
+            System.out.println(Thread.currentThread().getName()+"执行finally");
+            lock.unlock();
+            System.out.println(thread.getName()+"释放了锁");
+        }
+    }
+}
+
+class MyThread extends Thread {
+    private LockTest test = null;
+    public MyThread(LockTest test) {
+        this.test = test;
+    }
+    @Override
+    public void run() {
+
+        try {
+            test.insert(Thread.currentThread());
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName()+"被中断");
+        }
+    }
+}
